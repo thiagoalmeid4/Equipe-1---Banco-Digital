3aa9816d1e20e089c6c212139ec7790659e6e950
joaovitorscarvalho2007@gmail.com <jcarvalho@foursys.com.br>
2024-01-15 15:55:04 -0300

    adicionado classe Cartao

---
+++ src/main/java/br/com/fourbank/Cartao.java
@@ -0,0 +1,46 @@
+package br.com.fourbank;
+
+public class Cartao {
+
+	private Integer nCartao;
+	private String tipoCartao;
+	private String dataValidade;
+	private Integer codigoSeguranca;
+	
+	public Cartao() {}
+	
+	public Cartao(Integer nCartao, String tipoCartao, String dataValidade, Integer codigoSeguranca) {
+		super();
+		this.nCartao = nCartao;
+		this.tipoCartao = tipoCartao;
+		this.dataValidade = dataValidade;
+		this.codigoSeguranca = codigoSeguranca;
+	}
+	
+	public Integer getnCartao() {
+		return nCartao;
+	}
+	public void setnCartao(Integer nCartao) {
+		this.nCartao = nCartao;
+	}
+	public String getTipoCartao() {
+		return tipoCartao;
+	}
+	public void setTipoCartao(String tipoCartao) {
+		this.tipoCartao = tipoCartao;
+	}
+	public String getDataValidade() {
+		return dataValidade;
+	}
+	public void setDataValidade(String dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+	public Integer getCodigoSeguranca() {
+		return codigoSeguranca;
+	}
+	public void setCodigoSeguranca(Integer codigoSeguranca) {
+		this.codigoSeguranca = codigoSeguranca;
+	}
+	
+	
+}

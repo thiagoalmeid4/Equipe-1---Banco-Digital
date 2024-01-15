27e9647967825bfc69e9130a6f24def078edfc75
joaovitorscarvalho2007@gmail.com <jcarvalho@foursys.com.br>
2024-01-15 15:47:45 -0300

    adicionado classe Pedido

---
+++ src/main/java/br/com/fourbank/Pedido.java
@@ -0,0 +1,54 @@
+package br.com.fourbank;
+
+public class Pedido {
+
+	private Integer id;
+	private Double frete;
+	private Double valorTotal;
+	private String dataDoPedido;
+	private Boolean status;
+	
+	public Pedido() {}
+	
+	public Pedido(Integer id, Double frete, Double valorTotal, String dataDoPedido, Boolean status) {
+		super();
+		this.id = id;
+		this.frete = frete;
+		this.valorTotal = valorTotal;
+		this.dataDoPedido = dataDoPedido;
+		this.status = status;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Double getFrete() {
+		return frete;
+	}
+	public void setFrete(Double frete) {
+		this.frete = frete;
+	}
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	public String getDataDoPedido() {
+		return dataDoPedido;
+	}
+	public void setDataDoPedido(String dataDoPedido) {
+		this.dataDoPedido = dataDoPedido;
+	}
+	public Boolean getStatus() {
+		return status;
+	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	
+	
+	
+}

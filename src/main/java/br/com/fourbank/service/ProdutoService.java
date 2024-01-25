8d2bd188bf6a76555a128717ab74d2d494027334
EduardoBabetto <edubabetto@gmail.com>
2024-01-25 16:50:28 -0300

    Feita validações do código do produto

--- src/main/java/br/com/fourbank/service/ProdutoService.java
+++ src/main/java/br/com/fourbank/service/ProdutoService.java
@@ -6,31 +6,32 @@ import br.com.fourbank.dao.ProdutoDao;
 import br.com.fourbank.entity.Produto;
 
 public class ProdutoService {
-	
+
 	ProdutoDao produtoDao = new ProdutoDao();
-	
+
 	public void cadastrar(Produto a) {
-		if(a.getCodigo().length()== 8){
-			produtoDao.cadastrar(a);
+		if (a.getCodigo().length() == 8) {
+			Produto p = produtoPorCodigo(a.getCodigo());
+			if (p == null)
+				produtoDao.cadastrar(a);
+			else
+				throw new RuntimeException("Codigo existente");
 		}
-		
+
 	}
-  
-	public List<Produto> listar(){
+
+	public List<Produto> listar() {
 		return produtoDao.listar();
 	}
- 
 
 	public Produto produtoPorCodigo(String codigo) {
 		for (Produto p : produtoDao.listar()) {
-			if(p.getCodigo().equals(codigo)) {
+			if (p.getCodigo().equals(codigo)) {
 				return p;
 			}
-		
-		}	
+
+		}
 		return null;
 	}
-
-
 
 }

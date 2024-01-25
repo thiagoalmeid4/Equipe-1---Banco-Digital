2451704793b31d3f0c231bacf52faf7c8fea8832
Nantes07 <hnantes@foursys.com.br>
2024-01-25 17:19:31 -0300

    adicionado remover quantidade de produto

--- src/main/java/br/com/fourbank/dao/ProdutoDao.java
+++ src/main/java/br/com/fourbank/dao/ProdutoDao.java
@@ -8,14 +8,24 @@ import br.com.fourbank.entity.Produto;
 public class ProdutoDao {
 
 	ArrayList<Produto> produtos = new ArrayList<>();
-	
+
 	public String cadastrar(Produto a) {
 		produtos.add(a);
-		
-		return "produto "+a.getNome()+" adicionado";
+
+		return "produto " + a.getNome() + " adicionado";
 	}
-	
-	public List<Produto> listar(){
+
+	public List<Produto> listar() {
 		return produtos;
+	}
+
+	public void removerQtd(Produto produto) {
+		for (Produto p : produtos) {
+			if (p.getCodigo().equals(produto.getCodigo())) {
+				produto.setQuantidade(produto.getQuantidade() - 1);
+			}
+
+		}
+
 	}
 }
